b9cd8e74a179cc8b339d6b5f659b7229b0b6fb34
huawei <huawei@LAPTOP-HGQSTGRC>
2023-07-10 11:38:13 +0300

    Add project files.

---
+++ Zoo-school project/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Zoo_school_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string info = "Animal_Info.txt";
+            try
+            {
+
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File was not found please try again. ");
+            }
+        }
+        static void PrintMenu()
+        {
+            Console.WriteLine("1. Add new animal in the zoo");
+            Console.WriteLine("2. Change animal status");
+            Console.WriteLine("3. Check availability and animal info");
+            Console.WriteLine("4. Check all animals in the zoo");
+        }
+        static void chooseMenu()
+        {
+            Console.WriteLine("Choose a number(1-4).");
+            string n = Console.ReadLine();
+            switch (n)
+            {
+                case "1":
+                    Addanimal();
+                    break;
+                case "2":
+                    ChangeStatusOfAnimal();
+                    break;
+                case "3":
+                   CheckNumberAndInfoAboutAnimal();
+                    break;
+                case "4":
+                    ListOfAnimals();
+                    break;
+                default:
+                    break;
+            }
+        }
+        static void Addanimal()
+        {
+
+        }
+        static void ChangeStatusOfAnimal()
+        {
+
+        }
+        static void CheckNumberAndInfoAboutAnimal()
+        {
+
+        }
+        static void ListOfAnimals()
+        {
+
+        }
+    }
+}
---
+++ Zoo-school project/animals.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo_school_project
+{
+    class Animals
+    {
+        string animalID;
+        string species;
+        string name;
+        int age;
+        string habitat;
+        bool availability;
+
+        public Animals(string animalID, string species, string name, int age, string habitat, bool availability)
+        {
+            this.AnimalID = animalID;
+            this.Species = species;
+            this.Name = name;
+            this.Age = age;
+            this.Habitat = habitat;
+            this.Availability = availability;
+        }
+
+        public string AnimalID { get => animalID; set => animalID = value; }
+        public string Species { get => species; set => species = value; }
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+        public string Habitat { get => habitat; set => habitat = value; }
+        public bool Availability { get => availability; set => availability = value; }
+    }
+}
